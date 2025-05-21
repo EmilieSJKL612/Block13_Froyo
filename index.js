dca160fc1ad281b2420f71c523c718627e2f0aed
SJKL612 <emily.chiang@colostate.edu>
2025-05-20 22:34:41 -0600

    Divide-Conquer-Glue into 4 tasks

---
+++ index.js
@@ -0,0 +1,29 @@
+
+//goals: 1. prompt user to input flovaors  2. convert input into array  3. count each flavor's appearance  4. output to browser console
+
+
+//goal 1
+const input = prompt("Enter a list of froyo flavors seperated by commas:");
+
+
+//goal 2
+//learning note: 1. https://www.w3schools.com/jsref/jsref_split.asp  2. https://www.w3schools.com/jsref/jsref_trim_string.asp  3. https://www.w3schools.com/jsref/jsref_tolowercase.asp 
+const flavors = input.split(",").map(flavor => flavor.trim().toLowerCase());
+
+
+//goal3
+const flavorCounts = {};
+for (let i = 0; i < flovors.length; i ++){
+    
+}
+  
+
+//goal4
+console.log("Froyo Flavor Counts:");
+for (const flavor in flavorCounts) {
+  const count = flavorCounts[flavor];
+  console.log(`${flavor}: ${count}`);
+}
+
+  
+
