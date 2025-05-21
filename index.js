869e9a3c022eb06be66172c75ccf5fdca274b591
SJKL612 <emily.chiang@colostate.edu>
2025-05-20 22:45:55 -0600

    updated the code after checking for syntax errors

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 //goals: 1. prompt user to input flovaors  2. convert input into array  3. count each flavor's appearance  4. output to browser console
 
 
-//goal 1
+//goal 1 ->observe previous WS's bottom part (provided; not to modify part)
 const input = prompt("Enter a list of froyo flavors seperated by commas:");
 
 
-//goal 2
+//goal 2 ->use the .map() learned in block 14
 //learning note: 1. https://www.w3schools.com/jsref/jsref_split.asp  2. https://www.w3schools.com/jsref/jsref_trim_string.asp  3. https://www.w3schools.com/jsref/jsref_tolowercase.asp 
 const flavors = input.split(",").map(flavor => flavor.trim().toLowerCase());
 
 
 //goal3
 const flavorCounts = {};
-for (let i = 0; i < flovors.length; i ++){
+for (let i = 0; i < flavors.length; i ++){
     let flavor = flavors[i];
 
-    if (flovorCounts[flavor]){
+    if (flavorCounts[flavor]){
         flavorCounts[flavor] += 1;
     }
     else {
